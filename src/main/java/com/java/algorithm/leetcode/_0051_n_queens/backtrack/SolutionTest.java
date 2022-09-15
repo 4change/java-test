0278916609162f3535de6edbbd4c9f5a43138f9d
4change <e_mail4gxf@163.com>
2022-09-15 11:29:06 +0800

    删除algorithm文件夹

--- src/main/java/com/java/algorithm/leetcode/_0051_n_queens/backtrack/SolutionTest.java
+++
@@ -1,9 +0,0 @@
-package com.java.algorithm.leetcode._0051_n_queens.backtrack;
-
-public class SolutionTest {
-
-	public static void main(String[] args) {
-		System.out.println(new Solution().solveNQueens(8));
-	}
-
-}
--- src/main/java/com/java/algorithm/leetcode/_0518_coin_change_2/dp/SolutionTest.java
+++
@@ -1,9 +0,0 @@
-package com.java.algorithm.leetcode._0518_coin_change_2.dp;
-
-public class SolutionTest {
-
-	public static void main(String[] args) {
-		System.out.println(new Solution().change(5, new int[] {1, 2, 5}));
-	}
-
-}
